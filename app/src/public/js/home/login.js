1a796fa0d1251590de9a2e1a56d0e59f9bc98d8a
HungKungE <jonsun59@gmail.com>
2022-12-20 15:13:35 +0900

    dom으로 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,19 @@
 "use strict";
 
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+const login = () => {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+
+    console.log(req);
+}
+
+loginBtn.addEventListener("click", login);
+
+console.log(id);
 console.log("hello");
-console.log("bye");
