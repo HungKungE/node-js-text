6c30fed55e8b4721985d98e30c196cb86c818093
HungKungE <jonsun59@gmail.com>
2022-12-27 18:44:20 +0900

    로그인 화면 꾸미기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
@@ -13,6 +13,11 @@ const output = {
 
 const process = {
     login: (req, res) => {
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
+        return res.json(response);
+        /*
         const id = req.body.id,
           psword = req.body.psword;
 
@@ -32,6 +37,7 @@ const process = {
         response.success =false;
         response.msg = "로그인에 실패하셨습니다.";
         return res.json(response);
+        */
     },
 };
 
